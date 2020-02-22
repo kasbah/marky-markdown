17d21dfc7b2179c8ef7a63f4a9d03f986f0e3f56
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-22 17:10:59 +0000

    Add name into heading links

--- test/headings.js
+++ test/headings.js
@@ -60,6 +60,11 @@ describe('headings', function () {
     assert($md("h1 a.anchor[href='#h1']").length)
   })
 
+  it('injects name attribute into the heading links', function () {
+    assert(~fixtures.dirty.indexOf('# h1'))
+    assert($md("h1 a[name='h1']").length)
+  })
+
   it('adds anchor class to added heading links (HTML)', function () {
     assert(~fixtures.htmlheading.indexOf('<h1>one space</h1>'))
     assert($html("h1 a.anchor[href='#one-space']").length)
