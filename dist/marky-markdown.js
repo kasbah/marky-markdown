4a07d063826dfbf4c3b209fb7b159c26fd9504d6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-22 15:32:52 +0000

    Build

--- dist/marky-markdown.js
+++ dist/marky-markdown.js
@@ -985,6 +985,7 @@ var headings = module.exports = function (md, options) {
       linkOpen.attrSet('id', headings.prefix + postfix)
       linkOpen.attrSet('class', headingAnchorClass)
       linkOpen.attrSet('href', '#' + postfix)
+      linkOpen.attrSet('name', postfix)
       linkOpen.attrSet('aria-hidden', 'true')
 
       // add new token objects as children of heading
@@ -1597,15 +1598,6 @@ tokenUtil.unemoji = function (token) {
     return assign(new Token(), token, {content: token.markup})
   }
   return token
-}
-
-// wrap the the array of tokens in a link with the given slug as its destination
-// the intent is to operate on the .children array of a token
-tokenUtil.wrap = function (tokens, slug) {
-  var linkOpen = new Token('link_open', 'a', 1)
-  var linkClose = new Token('link_close', 'a', -1)
-
-  linkOpen.attrs = [['href', '#' + slug], ['name', slug]]
 }
 
 // return true if the token is type 'text' or contains any such tokens (except
